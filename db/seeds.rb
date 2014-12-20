1b460d09b7733fca9656c10304b0df2d2cea1631
Grace Yu <grace.yu4444@gmail.com>
2014-12-20 12:13:59 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create first_name: 'Grace',
+  middle_name: 'River',
+  last_name: 'Yu',
+  email: 'grace.yu4444@gmail.com',
+  password: '1234',
+  password_confirmation: '1234',
+  wordpress: 'http://rivergracefish.wordpress.com/',
+  github: 'https://github.com/RiverGracefish',
+  course: '',
+  user_type: 'Student',
+  bio: ''
